c61789018680dccb1070a16595243f8f0f3a3e9d
cdestradab <cdestradab@gmail.com>
2023-04-11 01:14:26 -0500

    Page Setup DONE

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, Genre, Language, BookInstance
+from django.views.generic import CreateView, DetailView
 
 # Create your views here.
 def index(request):
-    return HttpResponse("HOLA CABALLERE")
+    
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_avail = BookInstance.objects.filter(status__exact="a").count()
+
+    context = {
+        'num_books' : num_books,
+        'num_instances' : num_instances,
+        'num_instances_avail' : num_instances_avail
+    }
+
+    return render(request, "catalog/index.html", context=context)
+
+class BookCreate(CreateView): #model_form.html
+    model = Book
+    fields = '__all__'
+
+class BookDetail(DetailView):
+    model = Book
+
