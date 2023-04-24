7d624dda0aa2f1c8f47d3448a5480dd7dc8b9099
cdestradab <cdestradab@gmail.com>
2023-04-24 01:44:43 -0500

    User profile DONE

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book, Author, Genre, Language, BookInstance
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
@@ -39,3 +39,12 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'catalog/signup.html'
+
+class CheckedOutBooksByUserView(LoginRequiredMixin, ListView):
+    # List all BookInstances BUT  I will filter based off currently logged in user session
+    model = BookInstance
+    template_name = 'catalog/profile.html'
+    paginate_by = 5 # 5 book instances per page
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).all()
